04c78d5cd333ac83d219f85d934c5169ccac3908
antmusumba <musumbaantony3@gmail.com>
2024-11-25 10:54:44 +0300

    Feat: add testing for isSorted

---
+++ function/IsSorted_test.go
@@ -0,0 +1,26 @@
+package push
+
+import "testing"
+
+var TestCases = []struct {
+	name     string
+	input    []int
+	expected bool
+}{
+	{"Test1", []int{1, 2, 3, 4, 5}, true},
+	{"Test2", []int{5, 4, 1, 9, 2}, false},
+	{"Test3", []int{8, 99, 100, 103, 600}, true},
+}
+
+func TestCheckStack(t *testing.T) {
+	for _, tc := range TestCases {
+		t.Run(tc.name, func(t *testing.T) {
+			expected := tc.expected
+			got := IsSorted(tc.input)
+
+			if expected != got {
+				t.Errorf("Expected %v but got %v", expected, got)
+			}
+		})
+	}
+}
